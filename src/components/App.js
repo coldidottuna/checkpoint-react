5bc36ecfb6d30961613153529e371dace5d929a4
coldidottuna <fs.bakayoko@gmail.com>
2022-05-02 12:28:40 +0000

    checkpoint-recat-premier-commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "./Form";
+
+function App() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,54 @@
+import "../styles/form.css";
+
+function Form() {
+  return (
+    // <div className="Form">
+    //   <header className="App-header">
+    //     <button className="primary">Test button</button>
+    //   </header>
+    // </div>
+    <div>
+      <h1> Formulaire React bootstrap</h1>
+      <form className="container">
+        <div className="container mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className=" container mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" for="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+export default Form;
